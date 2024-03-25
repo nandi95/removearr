7f5a627618b299aa551a841baa106a47621c0335
Nandor Kraszlan <nandor.kraszlan@gmail.com>
2024-03-25 11:35:39 +0000

    Add custom logger

--- deps.ts
+++ deps.ts
@@ -1,2 +1,3 @@
 export { config as dotenv } from "https://deno.land/x/dotenv@v3.2.2/mod.ts";
-export  { parseArgs } from "https://deno.land/std@0.207.0/cli/parse_args.ts";
+export { parseArgs } from "https://deno.land/std@0.207.0/cli/parse_args.ts";
+export { yellow, gray, cyan, bold, italic, red } from "https://deno.land/std@0.220.1/fmt/colors.ts";
---
+++ src/utils/logger.ts
@@ -0,0 +1,109 @@
+import config from "../utils/config.ts";
+import { yellow, gray, cyan, bold, italic, red } from '/deps.ts';
+
+/* eslint-disable @typescript-eslint/naming-convention,@typescript-eslint/prefer-enum-initializers */
+export enum LogLevels {
+    Debug,
+    Info,
+    Warn,
+    Error,
+    Fatal
+}
+
+/* eslint-enable @typescript-eslint/naming-convention,@typescript-eslint/prefer-enum-initializers */
+
+const prefixes = new Map<LogLevels, string>([
+    [LogLevels.Debug, 'DEBUG'],
+    [LogLevels.Info, 'INFO'],
+    [LogLevels.Warn, 'WARN'],
+    [LogLevels.Error, 'ERROR'],
+    [LogLevels.Fatal, 'FATAL']
+]);
+const noColor: (str: string) => string = (msg) => msg;
+// eslint-disable-next-line @typescript-eslint/no-extra-parens
+const colorFunctions = new Map<LogLevels, (str: string) => string>([
+    [LogLevels.Debug, (srt: string) => gray(srt)],
+    [LogLevels.Info, (srt: string) => cyan(srt)],
+    [LogLevels.Warn, (srt: string) => yellow(srt)],
+    [LogLevels.Error, (str: string) => red(str)],
+    [LogLevels.Fatal, (str: string) => italic(bold(red(str)))]
+]);
+
+function logger({
+    logLevel = LogLevels.Info,
+    name = 'Main'
+}: {
+    logLevel?: LogLevels;
+    name?: string;
+} = {}) {
+    function log(level: LogLevels, ...args: unknown[]) {
+        if (level < logLevel) return;
+        let color = colorFunctions.get(level);
+        if (!color) color = noColor;
+        const date = new Date();
+        const logValues: string[] = [
+            `[${date.toLocaleDateString('en-GB', { timeZone: config.timeZone })}` +
+            ` ${date.toLocaleTimeString('en-GB', { timeZone: config.timeZone })}]`,
+            color(prefixes.get(level) ?? 'DEBUG'),
+            name ? `${name} >` : '>',
+            // @ts-expect-error - js can handle the type juggling
+            ...args
+        ];
+        switch (level) {
+            case LogLevels.Debug:
+                if (!config.debug) return;
+
+                return console.debug(...logValues);
+            case LogLevels.Info:
+                return console.info(...logValues);
+            case LogLevels.Warn:
+                return console.warn(...logValues);
+            case LogLevels.Error:
+                return console.error(...logValues);
+            case LogLevels.Fatal:
+                return console.error(...logValues);
+            default:
+                return console.log(...logValues);
+        }
+    }
+
+    function setLevel(level: LogLevels) {
+        logLevel = level;
+    }
+
+    function debug(...args: unknown[]) {
+        log(LogLevels.Debug, ...args);
+    }
+
+    function info(...args: unknown[]) {
+        log(LogLevels.Info, ...args);
+    }
+
+    function warn(...args: unknown[]) {
+        log(LogLevels.Warn, ...args);
+    }
+
+    function error(...args: unknown[]) {
+        log(LogLevels.Error, ...args);
+    }
+
+    function fatal(...args: unknown[]) {
+        log(LogLevels.Fatal, ...args);
+    }
+
+    return {
+        log,
+        setLevel,
+        debug,
+        info,
+        warn,
+        error,
+        fatal
+    };
+}
+
+export const log = logger({
+    logLevel: config.debug ? LogLevels.Debug : LogLevels.Info
+});
+
+export default log;
